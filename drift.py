d31dd4cbc321c974db344065b9a84c220a0073ac
Marcos Wilson Ogata <marcos.ogata@hotmail.com>
2020-09-28 02:33:53 -0300

    drift.py

--- drift.py
+++ drift.py
@@ -14,7 +14,7 @@ u_r = pd.read_excel('deslocamentos_real.xlsx').to_numpy()
 ### MODELANDO O DRIFT PARA SISMO ARTIFICIAL ###
 
 drift_u_a = np.zeros((8,2000))
-drift_u_a[0,:] = u_a[0,:]
+drift_u_a[0,:] = u_a[138,:]
 
 for i in range (0,2000):
     for k in range (0,7):
@@ -24,7 +24,7 @@ for i in range (0,8):
     plt.figure(i,figsize=(12,4))   
     plt.plot(t,100*drift_u_a[i,:],'b')
     plt.xlabel('Tempo (s)'); plt.ylabel('Deslocamento relativo entre pavimentos (cm)')
-    plt.xlim(0,max(t))
+    plt.xlim(0,max(t)); plt.ylim(-0.20,0,20)
     plt.title(f'Deslocamento relativo entre o {i}º e o {i+1}º pavimentos na direção 0 DEG')
     plt.grid(True)
     plt.show()
